ad65606cac15ab1cb9e37623886be07366f4da78
Andreu <andreu.riba@gracia.lasalle.cat>
2025-12-03 16:53:01 +0100

    envio dados con caras

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    let numRecibido = 0
+    if (numRecibido > miNum) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(500)
+    } else {
+        if (numRecibido < miNum) {
+            basic.showIcon(IconNames.Happy)
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("Hola")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
+input.onButtonPressed(Button.B, function () {
+    miNum = randint(1, 6)
+    basic.showNumber(miNum)
+    radio.sendNumber(miNum)
+})
+let miNum = 0
 radio.setGroup(1)
