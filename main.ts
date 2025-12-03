2032cccf269548e79f9be41c1667915f5d6b70d2
Andreu <andreu.riba@gracia.lasalle.cat>
2025-12-03 17:30:36 +0100

    opción con caras

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (numRecibido > miNumero) {
+        basic.showIcon(IconNames.Sad)
+    } else if (numRecibido < miNumero) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("Hola")
 })
@@ -5,10 +20,10 @@ radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    let miNumero = 0
     numRecibido = randint(1, 6)
     basic.showNumber(miNumero)
     radio.sendNumber(miNumero)
 })
+let miNumero = 0
 let numRecibido = 0
 radio.setGroup(1)
