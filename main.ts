496480a27561986e5f76d5b1c835dc82252c31de
Andreu <andreu.riba@gracia.lasalle.cat>
2025-12-03 16:40:31 +0100

    enviar cadena de texto

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hola")
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
